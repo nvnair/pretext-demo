848b2e3a01ce3c92b6d3e0930b5547f241965426
nvnair <nanditavnair28@gmail.com>
2026-02-12 18:26:23 +0000

    Latest build deployed.

---
+++ course/lunr-pretext-search-index.js
@@ -0,0 +1,104 @@
+var ptx_lunr_search_style = "textbook";
+var ptx_lunr_docs = [
+{
+  "id": "syllabus",
+  "level": "1",
+  "url": "syllabus.html",
+  "type": "Section",
+  "number": "",
+  "title": "Syllabus",
+  "body": " Syllabus        Course Information  This is the syllabus for Demo PreTeXt Course (PTX 101, section 007) for Spring 2026. It is a 0 credit course.   Instructor  Prof. Levin, Ross Hall 2240D, oscar.levin@unco.edu .    Student Hours  Tuesday and Friday 10-12 on Zoom    Class meets  MWF 10-11 in Ross 2090    Course Description  This course is entirely fictional. There isn't even a catalog to pull a description from. But if there were, it would have a course description and that course description would be put here for students to read (but likely because the university administration requires it be included in a syllabus).    Prerequisite  TEX 100 or suitable score on the typesetting placement exam.    Textbook and course materials   PreTeXt for Instructors by Oscar Levin. You can view an accessible online copy of this free, open source textbook at .       Course Overview  The goal of this course is to... oh, who am I kidding. This isn't a real course, so why would I spend time writting a course overview? I suppose I should because it helps see an example.  A common thing you might see here is a list of topics we will cover. So the topics we will cover include,   How to write a syllabus in PreTeXt .    How to write worksheets and handouts in PreTeXt     How to spend too much time typing up examples.   There is more we could say here, but for now I will stop.      Assessments and Grades  Your grade in this course will be based on a combination of your effort to master the material and the number of learning targets you master during the course.  Unlike a traditional course in which you earn points for completing various tasks to various levels of success, grading in this course is done using standards based grading . One advantage of this method is that you will have multiple opportunities to demonstrate your mastery of particular standards, and what matters is how many of these standards you demonstrate mastery on by the end of the course.  We call each standard a learning target and list these next.   Learning targets  By the end of this course, you should be able to:   Create a new PreTeXt document, build it into both HTML and PDF output, and deploy it to GitHub Pages.    Organize a PreTeXt document using sections, subsections, and paragraphs.    Make up random learning targets just to demonstrate what they look like.       Assessment of Learning Targets  Three times during the semester we will have an in-class exam. Each exam will be your first opportunity to demonstrate mastery of learning targets related to material we have covered prior to the exam.  If, after completing the exam, there are learning targets from that exam you have not demonstrated mastery on, you may attempt to demonstrate mastery on that target again by taking a quiz during student hours.    Final Exam  The final exam will take place on No day in particular, 2026 . Probably more to say here...    Assessment of Mathematical Engagement  Your level of effort and engagement will be assessed through your participation in class and completion of a variety of homework assignments.   Participation  Every three weeks you will be asked to complete a short self assessment on Canvas measuring your level of participation both in and out of class. This, combined with your attendance and my perception of your level of participation, will be used to determine your participation grade (out of 100).    Daily Prep Assignments  Prior to most classes you will have a short reading assignment or preview activity to complete. These will be graded based on completion.    Practice Problems  To gain procedural fluency in the content of the course, each topic will be accompanied with a small set of practice problems. These will be machine graded, but you can attempt them as many times as you like prior to the due date.    Written Homework  Some description of written homework should go here.       Final Grades  Grades will be computed based on the number of learning targets you have demonstrated mastery on and your level of mathematical engagement, according to the following table.      A  B  C  D    M (or E) level Learning Targets  15  12  9  6    E level Learning Targets  8  5  2  0    Engagement Score  90%  80%  70%  60%    To receive a particular grade, you must meet or exceed the requirements for that grade in all three categories. If you do not meet the requirements for a D , your final grade will be an F.       Course Policies   Attendance  Attendance is expected and critical to your success, and you will be missed if you are absent. Showing up is a key to success in all aspects of life. At the same time, I realize that it may be necessary for you to miss some class meetings due to work, family commitments, and so forth. In that case, let me know of your absence, and we can come up with a plan to help you stay up-to-date.  One important note though: in this class, just like in every other class, missing a day of class, even with a perfectly valid excuse, does not excuse you from learning what was covered that day. As your instructor, I will try to help you catch up, but this will take more work than it would have if you were in class.    Late work  All assignments have due dates; these exist to help you manage your workload and help me manage getting you feedback in a timely manner. To help motivate you to keep these deadlines, turning in your work on or before the due date will have the following benefits:   Approximately 5% free points on the assignment (for most work that I manually grade).    You will be among the first students to receive feedback\/grades on the assignment.    Opportunity to revise submitted work for a better grade.    Less stress about upcoming work and easier\/better learning for the next topic in class.   Additionally, most assignments will have an absolute cut-off for late work one week after the official due date. This is intended to give you a little flexibility but also not let old work pile up on top of you; sometimes it is just better to start with a clean slate and dive into the next thing.  Of course, life can happen and you don't owe me an explanation for why you might occasionally miss a deadline. Just let me know if you will be handing in something late and when I can expect it. This should not occur regularly; if you are having trouble meeting deadlines consistently, let's chat and make a plan.    Classroom Decorum  Please be respectful of your and my time by staying off your cell phones when in class, arriving on time, and not packing up your things before the end of class.  Additionally, we expect that you will treat all members of our course community with respect and kindness. In doing mathematics, or almost anything worth doing in life, you are going to make many errors and false starts while becoming more proficient. Think, for example, of learning to play a musical instrument, or learning an athletic skill, or developing a friendship. We want to establish a classroom atmosphere where the inevitable struggles and mistakes become an opportunity to learn and grow; not an opportunity for embarrassment. Thus, please be constructive and polite in questioning your colleagues in class.    Academic Integrity  Don't cheat! It is expected that members of this class will observe strict policies of academic honesty in every aspect of this course. In particular, you are expected to solve homework problems by yourself or together with your group, and not find solutions online. In general, the university's policies and recommendations for academic misconduct will be followed.    Generative AI  Generative AI tools (such as ChatGPT, Google Gemini, or CoPilot) are everywhere and present exciting opportunities. Some tasks AI can help us with can clear the way for more important things; but AI can also rob us of the opportunity to do the important things ourselves. It is your responsibility to ensure that you use these tools with academic integrity.  What does this mean? Ask yourself what you hope to get out of this course. I really hope your answer involves learning new skills, becoming a better thinker, and understanding mathematics at a deeper level (even if these goals are in service to eventually getting a better paying and more fulfilling job).  AI tools might be able to help you achieve these goals. Here are a few example prompts that you might find useful and are completely inline with these goals.    I am trying to learn the concept . Give me a sample question and ask me to find the answer and explain how I found it to you. Then ask me some followup questions, acting like a student I am tutoring on the topic. Make sure I can explain everything sufficiently so you can understand the concepts involved.      My professor gave me this sample quiz question: . Generate 5 additional questions that are similar in style and difficulty and ask me to give you the answers. Don't give me solutions until I have done so, but point out any errors I might have made.      Here is an example from my textbook: . Make up a similar question and walk me through a solution step my step, asking me to ask questions after each step to make sure I really understand the process.       Warning: As good as AI has become recently, it is still not great at mathematics, so anything you get out of a generative AI tool should be approached with a fair amount of skepticism. This is also a good way to learn mathematics though: play find the mistake in what AI gives you!  To be clear, you should NOT use AI tools to shortcut the learning process. Getting someone else to do your work for you (even if that someone is a robot) is plagiarism and a violation of the academic integrity policy. If you have any doubt about whether you are using AI productively, I'd love to have a conversation about it with you.    "
+},
+{
+  "id": "sec-course-info-2",
+  "level": "2",
+  "url": "syllabus.html#sec-course-info-2",
+  "type": "Paragraph (with a defined term)",
+  "number": "",
+  "title": "",
+  "body": "Demo PreTeXt Course (PTX 101, section 007) "
+},
+{
+  "id": "sec-assessments-3",
+  "level": "2",
+  "url": "syllabus.html#sec-assessments-3",
+  "type": "Paragraph (with a defined term)",
+  "number": "",
+  "title": "",
+  "body": "standards based grading "
+},
+{
+  "id": "sec-assessments-4",
+  "level": "2",
+  "url": "syllabus.html#sec-assessments-4",
+  "type": "Paragraph (with a defined term)",
+  "number": "",
+  "title": "",
+  "body": "learning target "
+},
+{
+  "id": "notes-week-01",
+  "level": "1",
+  "url": "notes-week-01.html",
+  "type": "Section",
+  "number": "",
+  "title": "Week 1",
+  "body": " Week 1   This is an outline of the topics we covered in the first week of class.     Monday 8\/22      Wednesday 8\/24      Friday 8\/26     "
+},
+{
+  "id": "notes-week-02",
+  "level": "1",
+  "url": "notes-week-02.html",
+  "type": "Section",
+  "number": "",
+  "title": "Week 2",
+  "body": " Week 2   Monday      Wednesday      Friday     "
+},
+{
+  "id": "activity-01-intro-activity",
+  "level": "1",
+  "url": "activity-01-intro-activity.html",
+  "type": "Worksheet",
+  "number": "",
+  "title": "Introduction Activity",
+  "body": " Introduction Activity    This is the introduction to the activity.      This is the first exercise.    "
+},
+{
+  "id": "activity-01-intro-activity-3",
+  "level": "2",
+  "url": "activity-01-intro-activity.html#activity-01-intro-activity-3",
+  "type": "Worksheet Exercise",
+  "number": "1",
+  "title": "",
+  "body": "  This is the first exercise.   "
+},
+{
+  "id": "handouts",
+  "level": "1",
+  "url": "handouts.html",
+  "type": "Chapter",
+  "number": "",
+  "title": "Handouts",
+  "body": " Handouts    "
+},
+{
+  "id": "homework",
+  "level": "1",
+  "url": "homework.html",
+  "type": "Chapter",
+  "number": "",
+  "title": "Homework",
+  "body": " Homework    "
+}
+]
+
+var ptx_lunr_idx = lunr(function () {
+  this.ref('id')
+  this.field('title')
+  this.field('body')
+  this.metadataWhitelist = ['position']
+
+  ptx_lunr_docs.forEach(function (doc) {
+    this.add(doc)
+  }, this)
+})
